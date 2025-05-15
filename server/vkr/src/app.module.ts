b78b6844112059cf11e59535add2e32210cce4a1
Victoria Raznodumova <raznodumova@gmail.com>
2025-05-16 01:52:40 +0300

    swagger settings

--- server/vkr/src/app.module.ts
+++ server/vkr/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot({
+      type: 'postgres', //тип подключаемой БД
+      port: 5432, //порт      username: 'education', //имя пользователя
+      password: '6458', //пароль
+      host: 'localhost', //хост, в нашем случае БД развернута локально
+      synchronize: false, //отключаем автосинхронизацию(в противном случае при каждом перезапуске наша БД будет создаваться заново)
+      logging: 'all', //включим логирование для удобства отслеживания процессов
+	  entities: ['dist/**/*.entity{.ts,.js}'], //указываем путь к сущностям
+    }),
+
+],
   controllers: [AppController],
   providers: [AppService],
 })
--- server/vkr/src/main.ts
+++ server/vkr/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //добавила из лабы со второго курса
+  const config = new DocumentBuilder()
+    .setTitle('Education API')
+    .setVersion('1.0')
+    .build(); // Конфигурируем сборщик документации
+  const document = SwaggerModule.createDocument(app, config); // создаем апи документацию
+  SwaggerModule.setup('api_docs', app, document); //включаем документацию Swagger по пути localhost:3000/api_docs
+
+
+
+
+
   await app.listen(3000);
+  //добавила из лабы со второго курса
+  await app.setGlobalPrefix('/api'); //глобальный префикс для роутов контроллера
 }
 bootstrap();
